a63780c4fd10602a1253505932cc39b46602c1b1
Baran Özdemir <baranozdemir305@gmail.com>
2022-08-30 17:51:06 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Icons from './components/Icons';
+
+import logo from './logo.jpeg'
+import cat from './cat.jpeg'
+
+import instagramLogo from './instagram-logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='flex h-screen overflow-auto items-center justify-center'>
+        <div className='flex flex-col border border-[#3BC183] shadow-lg shadow-[#3BC183] rounded-2xl overflow-hidden'>
+          <div className='relative pl-20 pr-8'>
+            <div className='absolute left-0 w-14 h-14 bg-[#3BC183] flex items-center justify-center rounded-br-2xl'>
+              <Icons name='picture' className='w-6 h-6 text-white' />
+            </div>
+            <div className='mt-7'>
+              <div className='grid grid-cols-2'>
+                <div className='flex items-center justify-center'>
+                  <div className='relative'>
+                    <img alt='logo' src={logo} className="w-16 rounded-md border border-[#D6D6D6]" />
+                    <span className='absolute -right-2 -bottom-2'>
+                      <img alt='instagram' src={instagramLogo} className="w-5 h-5 rounded-full" />
+                    </span>
+                  </div>
+                  <div className='ml-3 flex items-start justify-center flex-col text-black'>
+                    <p>Social Orientation</p>
+                    <p className='text-[#9A9A9A] font-light'>17 June 2021 - 10:15</p>
+                  </div>
+                </div>
+                <div className='flex items-center justify-end gap-2'>
+                  <Icons name='ban' className='w-5 h-5 text-[#9A9A9A]' />
+                  <Icons name='duplicate' className='w-5 h-5 text-[#9A9A9A]' />
+                  <Icons name='edit' className='w-5 h-5 text-[#9A9A9A]' />
+                  <Icons name='trash' className='w-5 h-5 text-[#9A9A9A]' />
+                  <Icons name='dots' className='w-5 h-5 text-[#9A9A9A]' />
+                </div>
+              </div>
+            </div>
+            <div className='max-w-md my-8'>
+              <p className='text-black text-md'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mi tortor, porta eu egestas ac, ultricies sed leo. Sed id lorem orci. Vivamus eu pulvinar elit, in accumsan</p>
+            </div>
+          </div>
+          <div className='flex items-center justify-between'>
+            <div className='p-5'>
+              <ul className='flex flex-col gap-4'>
+                <li>
+                  <span>125</span>
+                  <Icons name='heart' className='text-[#9A9A9A]' />
+                </li>
+                <li>
+                  <span>125</span>
+                  <Icons name='comment' className='text-[#9A9A9A]' />
+                </li>
+                <li>
+                  <span>125</span>
+                  <Icons name='share' className='text-[#9A9A9A]' />
+                </li>
+                <li>
+                  <span>125</span>
+                  <Icons name='eye' className='text-[#9A9A9A]' />
+                </li>
+              </ul>
+            </div>
+            <div className='flex items-center justify-center pr-8 pb-7'>
+              <img src={cat} alt='cat' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Icons.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+
+interface IIconsProps {
+  name: string
+  className?: string
+}
+
+const IconHeart = (props: IIconsProps) => {
+  return (
+    <svg className={props.className} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+  )
+}
+
+const CommentIcon = (props: IIconsProps) => {
+  return (
+    <svg className={props.className} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" /></svg>
+  )
+}
+
+const ShareIcon = (props: IIconsProps) => {
+  return (
+    <svg className={props.className} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" /></svg>
+  )
+}
+
+const EyeIcon = (props: IIconsProps) => {
+  return (
+    <svg className={props.className} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" /></svg>
+  )
+}
+
+const PictureIcon = (props: IIconsProps) => {
+  return (
+    <svg className={props.className} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" /></svg>
+  )
+}
+
+const BanIcon = (props: IIconsProps) => {
+  return (
+    <svg className={props.className} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" /></svg> 
+  )
+}
+
+const DuplicateIcon = (props: IIconsProps) => {
+  return (
+    <svg className={props.className} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" /></svg>
+  )
+}
+
+const EditIcon = (props: IIconsProps) => {
+  return (
+    <svg className={props.className} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" /></svg>
+  )
+}
+
+const TrashIcon = (props: IIconsProps) => {
+  return (
+    <svg className={props.className} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>
+  )
+}
+
+const DotsIcon = (props: IIconsProps) => {
+  return (
+    <svg className={props.className} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" /></svg>
+  )
+}
+
+const Icons: React.FC<IIconsProps> = (props) => {
+
+  const icons = {
+    heart: IconHeart,
+    comment: CommentIcon,
+    share: ShareIcon,
+    eye: EyeIcon,
+    picture: PictureIcon,
+    ban: BanIcon,
+    duplicate: DuplicateIcon,
+    edit: EditIcon,
+    trash: TrashIcon,
+    dots: DotsIcon
+  }
+
+  const IconComponent = icons[props.name as keyof typeof icons] || null;
+
+  return <IconComponent {...props} />
+}
+
+export default Icons
